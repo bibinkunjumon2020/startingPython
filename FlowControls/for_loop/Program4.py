8567cbcda2a9d19e9c28edd283b7bda9995f1e5c
bibinkunjumon <bibinkunjumon2020@gmail.com>
2022-05-07 16:09:43 +0530

    prime number for loop

--- FlowControls/for_loop/Program4.py
+++ FlowControls/for_loop/Program4.py
@@ -5,7 +5,7 @@ flag='true'             # flag value normal is prime
 for i in range(2,number-1):
     if(number%i ==0):
         flag='false'   #flag value not prime
-        print("NUmber is not prime")
+        print("Number is not prime")
         break
 if(flag=='true'):
     print("Number is prime")
